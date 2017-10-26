31c8f9a3aceeee23239b7477fd1cef885fa3d8a0
inging44 <ltinghuang@163.com>
2017-10-26 17:14:19 +0800

    try 简易版api

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
     url(r'^index/', views.index),
+    url(r'^my/', views.my),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
